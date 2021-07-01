2c9ba6067c942a9aaa0aeb50e08d85b53754d498
yanarie123 <yanariestradatuhehay@gmail.com>
2021-07-01 08:13:07 +0000

    aw

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -2,8 +2,7 @@ import cloudscraper
 from bs4 import BeautifulSoup
 scraper = cloudscraper.CloudScraper()
 hasil = scraper.get("http://panel.jagoanssh.com/login")
-soup=BeautifulSoup(hasil.text, 'html.parser')
-hasil = soup.find_all('input', {'class': "_token"})
-h=hasil[0]['value']
-print(h)
-#print(parsing.find_all('value',class_='_token'))
+print(hasil.headers)
+#soup=BeautifulSoup(hasil.text, 'html.parser')
+#hasil = soup.find_all('input', {'class': "_token"})
+#h=hasil[0]['value']
